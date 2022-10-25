f819265145c68684c6bdfb5f2acab6fd50a28541
ojochau <ojochau@instajs.com>
2022-10-25 18:39:15 +0800

    handle get networks from scconfig

--- tmp/modules/main/index.tsx
+++ tmp/modules/main/index.tsx
@@ -1,19 +1,44 @@
-import {Module, Styles, Container, customModule} from '@ijstech/components';
+import { Module, Styles, Container, customModule } from '@ijstech/components';
 import Assets from '@dapp/assets';
 import styleClass from './index.css';
-export {Header} from './header';
+import { setDataFromSCConfig } from '@dapp/network';
+export { Header } from './header';
 Styles.Theme.applyTheme(Styles.Theme.darkTheme);
 
+interface IMenuItem {
+	caption?: string;
+  module: string;
+}
+
 @customModule
-export default class MainLauncher extends Module{
-    constructor(parent?: Container, options?: any) {
+export default class MainLauncher extends Module {
+	private menuItems: any[];
+	constructor(parent?: Container, options?: any) {
 		super(parent, options);
 		this.classList.add(styleClass);
 	};
-    getBreadcrumbList(): string[] {
+	override initCustomData(options: any) {
+		this.menuItems = options.menus || [];
+		setDataFromSCConfig(options);
+	}
+	getBreadcrumbList(): string[] {
 		const url = window.location.hash.replace('#/', '');
 		return url.split('/').map(v => decodeURIComponent(v.split("?")[0]));
 	}
+	// getMenus(options?: any) {
+	// 	if (!options) return [];
+	// 	return options.menus.map((menu: IMenuItem) => {
+	// 		let modulePath = menu.module;
+	// 		if (options.modules && options.modules[menu.module] && options.modules[menu.module].path) {
+	// 			modulePath = "/";
+	// 			if (options.moduleDir)
+	// 					modulePath += options.moduleDir + '/';
+	// 			modulePath += options.modules[menu.module].path;
+	// 			if (!modulePath.endsWith('.js'))
+	// 					modulePath += '/index.js';
+	// 		}
+	// 	})
+	// }
 
 	// async renderBreadcrumb() {
 	// 	if (!this.header || !this.header.breadcrumb) return;
@@ -52,18 +77,18 @@ export default class MainLauncher extends Module{
 	// 	this.breadcrumbStack.visible = true;
 	// 	this.breadcrumbStack.append(...breadcrumbs);
 	// }
-    render(){
-        <i-panel>
-            <i-stack
-                id="mainPanel"
-                height="800"
-                width="100%"
-                position="absolute"
-                dock='top'
-                zIndex={-10}
-            ></i-stack>
-            <main-header id="header" width="100%"></main-header>
-            <i-panel id="footer" width="100%"></i-panel>
-        </i-panel>
-    };
+	render() {
+		<i-panel>
+			<i-stack
+				id="mainPanel"
+				height="800"
+				width="100%"
+				position="absolute"
+				dock='top'
+				zIndex={-10}
+			></i-stack>
+			<main-header id="header" width="100%" menuItems={this.menuItems}></main-header>
+			<i-panel id="footer" width="100%"></i-panel>
+		</i-panel>
+	};
 };
