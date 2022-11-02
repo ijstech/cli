06f08e499d65222926c70ab2e0169d0205ff1a10
instajs <ycwong@instajs.com>
2022-11-02 23:35:43 +0800

    update dapp template

--- tools/templates/dapp/modules/assets/index.ts
+++ tools/templates/dapp/modules/assets/index.ts
@@ -5,26 +5,6 @@ function fullPath(path: string): string{
     return `${moduleDir}/${path}`
 };
 export default {
-    fonts: {
-        poppins: {
-            bold: fullPath('fonts/poppins/PoppinsBold.ttf'),
-            italic: fullPath('fonts/poppins/PoppinsItalic.ttf'),
-            light: fullPath('fonts/poppins/PoppinsLight.ttf'),
-            medium: fullPath('fonts/poppins/PoppinsMedium.ttf'),
-            regular: fullPath('fonts/poppins/PoppinsRegular.ttf'),
-            thin: fullPath('fonts/poppins/PoppinsThin.ttf'),
-        }
-    },
-    img: {
-        network: {
-            bsc: fullPath('img/network/bsc.svg'),
-            eth: fullPath('img/network/eth.svg'),
-        },
-        wallet: {
-            metamask: fullPath('img/wallet/metamask.png'),
-            trustwallet: fullPath('img/wallet/trustwallet.svg'),
-            binanceChainWallet: fullPath('img/wallet/binance-chain-wallet.svg'),
-        }
-    },    
+    logo: fullPath('img/logo.svg'),
     fullPath
 };
