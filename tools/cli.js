f2948ae34329a63ad1a9b96a06f15d0a3512095d
instajs <ycwong@instajs.com>
2022-11-18 16:20:31 +0800

    add bundle deployer

--- tools/cli.js
+++ tools/cli.js
@@ -3,35 +3,44 @@ const Fs = require('fs');
 const Path = require('path');
 const RootDir = process.cwd();
 
-async function copy(source, target ) {    
-    if (Fs.lstatSync(source).isDirectory() ) {
-        let files = Fs.readdirSync(source);
-        files.forEach(function (file) {
-            copy(Path.join(source, file), Path.join(target, file))
-        } );
-    }
-    else if (!Fs.existsSync(target)){
-        try{
-            Fs.mkdirSync(Path.dirname(target), { recursive: true });
-        }
-        catch(err){}
-        Fs.writeFileSync(target, Fs.readFileSync(source));
-    }
-}
+async function copyDir(source, target ) {    
+    let files = Fs.readdirSync(source);
+    files.forEach(async function (file) {
+        if (Fs.lstatSync(Path.join(source, file)).isDirectory())
+            Fs.cpSync(Path.join(source, file), Path.join(target, file), {recursive: true})
+        else
+            Fs.copyFileSync(Path.join(source, file), Path.join(target, file))
+    });
+};
 async function main(){
     let args = process.argv.slice(2);
-    if (args[0] == 'init'){
+    if (args[0] == 'bundle'){
+        if (args[1] == 'deployer'){
+            let packageJson = JSON.parse(Fs.readFileSync(Path.join(RootDir, 'package.json')));
+            let packagePath = Path.dirname(require.resolve('@scom/contract-deployer'));
+            let deployerPath = Path.join(RootDir, 'deployer');
+            Fs.mkdirSync(deployerPath, {recursive: true});            
+            await copyDir(packagePath, deployerPath);
+            Fs.mkdirSync(Path.join(deployerPath, 'libs', packageJson.name), {recursive: true});
+            let scconfig = JSON.parse(Fs.readFileSync(Path.join(packagePath, 'scconfig.json')));
+            scconfig.contract = packageJson.name;
+            scconfig.dependencies[packageJson.name] = '*';
+            Fs.writeFileSync(Path.join(deployerPath, 'scconfig.json'), JSON.stringify(scconfig, null, 4));
+            Fs.copyFileSync(Path.join(RootDir, 'dist/index.js'), Path.join(deployerPath, 'libs', packageJson.name + '/index.js'))
+        }
+    }
+    else if (args[0] == 'init'){
         if (args[1] == 'worker'){
-            copy(Path.join(__dirname, 'templates/worker'), RootDir)
+            copyDir(Path.join(__dirname, 'templates/worker'), RootDir)
         }
         else if (args[1] == 'router'){
-            copy(Path.join(__dirname, 'templates/router'), RootDir)
+            copyDir(Path.join(__dirname, 'templates/router'), RootDir)
         }
         else if (args[1] == 'contract'){
-            copy(Path.join(__dirname, 'templates/contract'), RootDir)
+            copyDir(Path.join(__dirname, 'templates/contract'), RootDir)
         }
         else if (args[1] == 'dapp'){
-            copy(Path.join(__dirname, 'templates/dapp'), RootDir)
+            copyDir(Path.join(__dirname, 'templates/dapp'), RootDir)
         }
         let packPath = Path.join(RootDir, 'package.json');
         if (args[2] && Fs.existsSync(packPath)){
@@ -39,7 +48,7 @@ async function main(){
             pack.name = args[2];
             Fs.writeFileSync(packPath, JSON.stringify(pack, null, 4));
         };
-    }
+    }    
     else if (args[0] == 'serve'){
         let serve = require('./serve');
         serve(args[1] || 8080, args[2] || 'dist');
