b00161eed0e4eeebbe06a32625128ea87330f827
instajs <ycwong@instajs.com>
2022-11-18 16:08:48 +0800

    update contract template

--- tools/templates/contract/src/index.ts
+++ tools/templates/contract/src/index.ts
@@ -1,2 +1,45 @@
 import * as Contracts from './contracts/index';
-export default Contracts;
+export {Contracts};
+import {IWallet, BigNumber} from '@ijstech/eth-wallet';
+
+export interface IDeployOptions {
+    initSupply?: string;
+};
+export interface IDeployResult {
+    erc20: string;
+};
+var progressHandler: any;
+export var DefaultDeployOptions: IDeployOptions = {
+    initSupply: '10000'
+};
+function progress(msg: string){
+    if (typeof(progressHandler) == 'function'){
+        progressHandler(msg);
+    };
+}
+export async function deploy(wallet: IWallet, options?: IDeployOptions): Promise<IDeployResult>{
+    progress('Contracts deployment start');
+    let erc20 = new Contracts.ERC20(wallet);
+    progress('Deploy ERC20');
+    let address = await erc20.deploy();
+    progress('ERC20 deployed ' + address)
+    if (options && options.initSupply){
+        progress('Mint initial supply ' + options.initSupply)
+        let value = new BigNumber(options.initSupply);
+        let result = await erc20.mint(value);
+        progress('Transaction # ' + result.transactionHash);
+    };
+    progress('Contracts deployment finished');
+    return {
+        erc20: address
+    };
+};
+export function onProgress(handler: any){
+    progressHandler = handler;
+};
+export default {
+    Contracts,
+    deploy,
+    DefaultDeployOptions,
+    onProgress
+};
--- tools/templates/contract/test/erc20.test.ts
+++ tools/templates/contract/test/erc20.test.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import Ganache from "ganache";
 import {Wallet} from "@ijstech/eth-wallet";
-import Contracts from "../src/index";
+import {Contracts, deploy} from "../src/index";
 import assert from "assert";
 
 describe('##Contracts', function() {
@@ -17,14 +17,12 @@ describe('##Contracts', function() {
         });
         wallet = new Wallet(provider);
         accounts = await wallet.accounts;
-        console.log(accounts);
         wallet.defaultAccount = accounts[0];
     })
     it('erc20', async function() {
-        let erc20 = new Contracts.ERC20(wallet);
-        let address = await erc20.deploy();
-        console.dir('#deployed ERC20 address: ' + address);
-        await erc20.mint(1000);        
+        let result = await deploy(wallet, {initSupply: '1000'});
+        let erc20 = new Contracts.ERC20(wallet, result.erc20);
+        console.dir('#deployed ERC20 address: ' + result.erc20);
         await erc20.transfer({
             recipient: accounts[1],
             amount: 100
