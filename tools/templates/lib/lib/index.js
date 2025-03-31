4d7449c5a832371ca732d1cee477bcd16e0d773f
YC Wong <66290913+yc-wong@users.noreply.github.com>
2025-03-31 19:57:25 +0800

    update lib template

--- tools/templates/lib/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.demoFunction = void 0;
-function demoFunction() {
-    return 'Hello';
-}
-exports.demoFunction = demoFunction;
-;
--- tools/templates/lib/pluginTypes/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <amd-module name="demo-lib" />
-declare module "demo-lib" {
-    export function demoFunction(): string;
-}
